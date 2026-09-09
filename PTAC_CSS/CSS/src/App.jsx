3db898efdf42abbb465cdbd6f6a0399b4525edf4
JuaumQxC <joaoaugusto3235@gmail.com>
2026-09-09 20:12:38 +0000

    sd

---
+++ PTAC_CSS/CSS/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+export default function App(){
+  return(
+    <>
+    <h1>Titulo do SIte</h1>
+    <p>PROGRAMAÇÂO E TECNOLOGIAS PARA APLICAÇÕES CLIENTE 2</p>
+    <ul>
+      <li>Item 0</li>
+      <li>Item 1</li>
+      <li>Item 2</li>
+    </ul>
+    <Arquivo titulo="Teste titulo"/>
+    </>
+  )
+}
---
+++ PTAC_CSS/CSS/src/Components/Arquivo.jsx
@@ -0,0 +1,8 @@
+import './Arquivo.css'
+export default function Arquivo (){
+    return(
+        <article>
+            <h1 className="titulo">{props.titulo}</h1>
+        </article>
+    )
+}
